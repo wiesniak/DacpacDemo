320b10b3134cc152be7a721303188af6a3256581
wiesniak <wiesniak_xl@wp.pl>
2023-03-02 23:54:15 +0100

    Add demo code.

---
+++ DacpacDemo.Database/Views/DemoView.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW dbo.DemoView
+AS
+    SELECT
+        CatalogName = FTC.[name],
+        caFTC.IndexSize
+    FROM sys.fulltext_catalogs AS FTC
+        CROSS APPLY (
+            SELECT
+                IndexSize = FULLTEXTCATALOGPROPERTY(FTC.[name], 'IndexSize')
+        ) AS caFTC
+GO
